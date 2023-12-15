f1091bcd693529b6de05159930c6a5b8bcbb6f64
Raymond <raymondw85@nycstudents.net>
2023-12-15 10:04:59 -0500

    added more print statments

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("hello period 3");
         System.out.println("today is a wonderful friday");
         System.out.println("one more");
+        System.out.println("Somebody once told me the world was gonna roll me");
+        System.out.println("I ain't the sharpest tool in the shed");
     }
 }
