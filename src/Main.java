543cb4e747c849227ab7622c4da98522fdd2f982
laptop <sample@bths.edu>
2023-12-15 09:59:30 -0500

    added print

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello period 3");
         System.out.println("today is a wonderful friday");
+        System.out.println("one more");
     }
 }
