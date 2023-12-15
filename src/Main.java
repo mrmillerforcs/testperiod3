557e9944bc69365c134ec6471e241ef20b4d86f5
Raymond <raymondw85@nycstudents.net>
2023-12-15 09:55:55 -0500

    Added print statement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello period 3");
+        System.out.println("today is a wonderful friday");
     }
 }
